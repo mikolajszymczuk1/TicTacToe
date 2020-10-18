7e7f18c43d35cef451903ec2c1f7a1aa250d961a
Mikołaj <mikolajszymczuk.dev@wp.pl>
2020-10-18 14:26:22 +0200

    Add game logic

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,48 @@
 import "@fortawesome/fontawesome-free/css/all.css";
+import { createCircleCross, whoFirst, nextPlayer, clearBoard, somebodyWin } from "./functions";
+
+// Get game elements
+const board = document.querySelector(".game__board");
+const cells = document.querySelectorAll(".game__board-cell");
+const message = document.querySelector(".game__messages");
+const clear_button = document.querySelector(".clear-b");
+const theme_button = document.querySelector(".theme-b");
+
+// Possible win positions
+const winMoves = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+let player = whoFirst();
+let canMove = true;
+
+// Interactions
+board.addEventListener("click", (e) => { gameController(e); });
+clear_button.addEventListener("click", () => { 
+    clearBoard(cells);
+    message.innerText = "O vs X";
+    canMove = true;
+});
+
+// Main game controller
+function gameController(e) {
+    if (canMove) {
+        if (!e.target.hasChildNodes()) {
+            createCircleCross(player, e.target);
+
+            if (somebodyWin(winMoves, cells, player)) {  // Check if somebody win
+                message.innerText = player + " win !!!";
+                canMove = false;
+            }
+
+            player = nextPlayer(player);
+        }
+    }
+}
---
+++ src/js/functions.js
@@ -0,0 +1,54 @@
+// Create circle or cross and add to parent
+function createCircleCross(t, p) {
+    let div = document.createElement("div");
+
+    if (t === "O") {
+        div.classList.add("game__circle");
+    } else if (t === "X") {
+        div.classList.add("game__cross");
+    }
+
+    p.dataset.cell = t;
+    p.appendChild(div);
+}
+
+// Check if somebody is winner
+function somebodyWin(winMoves, cells, player) {
+    for (let win of winMoves) {
+        if ((cells[win[0]].dataset.cell === player) && (cells[win[1]].dataset.cell === player) && (cells[win[2]].dataset.cell === player)) {
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+// Pick a player who will be first
+function whoFirst() {
+    const num = Math.floor(Math.random() * 2);
+
+    if (num === 0) {
+        return "O";
+    }
+
+    return "X";
+}
+
+// Change to next player
+function nextPlayer(p) {
+    if (p === "O") {
+        return "X";
+    }
+
+    return "O";
+}
+
+// Clear board
+function clearBoard(cells) {
+    for (let c of cells) {
+        c.innerHTML = "";
+        c.dataset.cell = "";
+    }
+}
+
+export { createCircleCross, whoFirst, nextPlayer, clearBoard, somebodyWin};
